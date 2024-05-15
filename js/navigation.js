98f50e1623dccd40653ff801dcc47b268d4c2065
Anastasia Dubinina <anastasiadubinina1123@gmail.com>
2024-05-15 12:47:02 +0300

    Added navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -1,27 +1,52 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const generalLink = document.querySelector('.general-link');
-    const servicesLink = document.querySelector('.services-link');
-    const galleryLink = document.querySelector('.gallery-link');
-    const contactLink = document.querySelector('.contact-link');
-    const services = document.querySelector('.services');
-    const gallery = document.querySelector('.gallery');
-    const contact = document.querySelector('.contact');
+    const buttons = document.querySelector('.menu__list');
+    const services = document.querySelector('.services').getBoundingClientRect().top;
+    const gallery = document.querySelector('.gallery').getBoundingClientRect().top;
+    const contact = document.querySelector('.contact').getBoundingClientRect().top;
+    
+    // нужно изменить координаты чтобы прокручивалось на верхнюю границу секций с учетом высоты хэдэра
+    
+    function scrollTo() {
+        buttons.addEventListener('click', function (event) {
+            event.preventDefault();
 
+            let target = event.target;
+            
+            if(target && target.classList.contains('general-link')) {
+                window.scrollTo({
+                    top: 0,
+                    left: 0,
+                    behavior: "smooth",
+                });
+            }
 
+            if(target && target.classList.contains('services-link')) {
+                window.scrollTo({
+                    top: services,
+                    left: 0,
+                    behavior: "smooth",
+                });
+            }
 
-    function scrollToGeneral() {
-        generalLink.addEventListener('click', () => {
-            // document.documentElement.scrollTop = 0;
+            if(target && target.classList.contains('gallery-link')) {
+                window.scrollTo({
+                    top: gallery,
+                    left: 0,
+                    behavior: "smooth",
+                });
+            }
 
-            window.scrollBy({
-                top: 0,
-                behavior: "smooth"
-            });
+            if(target && target.classList.contains('contact-link')) {
+                window.scrollTo({
+                    top: contact,
+                    left: 0,
+                    behavior: "smooth",
+                });
+            }
         });
-    };
+    }
 
-    scrollToGeneral();
-
+    scrollTo();
 });
